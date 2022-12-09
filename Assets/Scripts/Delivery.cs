de1ffddbf009311324d950d1f5ef8542bf3f44f7
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2022-12-08 20:25:35 -0500

    Initial Player + controls

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    Color32 HasPackageColor = new Color32(1, 1, 1, 1);
+    Color32 NoPackagaeColor = new Color32(1, 1, 1, 1);
+
+    float DestroyDelay = .05f;
+}
---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    public float SteerSpeed = 100f, MoveSpeed = 20f, SlowSpeed = 15f, BoostSpeed = 30f;
+
+    private void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * SteerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        MoveSpeed = SlowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            MoveSpeed = BoostSpeed;
+        }
+    }
+}
